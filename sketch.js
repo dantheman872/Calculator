5567c71851e87bd8a2ef3570f40147d2bae1bb47
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2025-10-21 17:34:32 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-const ROWS = 14
-const COLS = 14
+const ROWS = 11
+const COLS = 11
 
 function setup(){
 
@@ -7,74 +7,97 @@ function setup(){
     textAlign(CENTER)
 }
 
-let inputA = new Array(COLS).fill(false)
-let inputB = new Array(COLS).fill(false)
-let outputA = new Array(COLS).fill(false)
-let carryA = new Array(COLS).fill(false)
+let setting = new Array(COLS).fill(false)
+let inputA = new Array(COLS-3).fill(false)
+let inputB = new Array(COLS-3).fill(false)
+let outputA = new Array(COLS-2).fill(false)
+let carryA = [false]
 
 function draw(){
 
     background(255)
     rectWidth = width / COLS
-    addBit(0)
-    addBit(90)
-    addBit(180)
+    addBit(11,0)
+    addBit(8,180)
+    addBit(8,270)
+    addBit(9,450)
 
-    updateBit(inputA, 0)
-    updateBit(inputB, 90)
+    updateBit(setting, 0, 0)
+    updateBit(inputA, 180, 3)
+    updateBit(inputB, 270, 3)
     
     mousePressed()
     mouseReleased()
     bitAddition()
     fill(25)
-    // Text under InputA
+
+    textSize(10)
+    // Text under settings
     for(let i = 0; i < COLS; i++){
+        let settingText = ['Add','Subtract','AND','NAND','OR','NOR','XOR','XNOR','IMPLIES','NIMPLIES','Right Shift']
+        text(settingText[i], (rectWidth /2) + i * rectWidth, 80)
+    }
+
+    textSize(20)
+    // Text under InputA
+    for(let i = 0; i < COLS - 3; i++){
         let x = i
-        text(2 ** i,(rectWidth / 2) + i * rectWidth, 85)
+        text(2 ** i,(rectWidth / 2) + i * rectWidth, 260)
     }
 
     // Text under InputB
-    for(let i = 0; i < COLS; i++){
+    for(let i = 0; i < COLS - 3; i++){
 
-        text(2 ** i,(rectWidth / 2) + i * rectWidth, 175)
+        text(2 ** i,(rectWidth / 2) + i * rectWidth, 350)
     }
 
     //Text under OutputA
-    for(let i = 0; i < COLS; i++){
+    for(let i = 0; i < COLS - 2; i++){
 
-        text(2 ** i,(rectWidth / 2) + i * rectWidth, 265)
+        text(2 ** i,(rectWidth / 2) + i * rectWidth, 530)
     }
 }
 
-function addBit(y){
+function addBit(l, y){
 
-    for(let i = 0; i < COLS; i++){
-        if (inputA[i] == true && y == 0){
+    for(let i = 0; i < l; i++){
+
+        if (setting[i] == true && y == 0){
 
             fill(225,255,23)
-        }
+        } 
 
-        if (inputA[i] == false && y == 0){
+        if (setting[i] == false && y == 0){
 
             fill(175)
         }
 
-        if (inputB[i] == true && y == 90){
+        if (inputA[i] == true && y == 180 && i < l){
 
             fill(225,255,23)
         }
 
-        if (inputB[i] == false && y == 90){
+        if (inputA[i] == false && y == 180 && i < l){
 
             fill(175)
         }
 
-        if (outputA[i] == true && y == 180){
+        if (inputB[i] == true && y == 270 && i < l){
 
             fill(225,255,23)
         }
 
-        if (outputA[i] == false && y == 180){
+        if (inputB[i] == false && y == 270 && i < l){
+
+            fill(175)
+        }
+
+        if (outputA[i] == true && y == 450){
+
+            fill(225,255,23)
+        }
+
+        if (outputA[i] == false && y == 450){
 
             fill(175)
         }
@@ -82,9 +105,9 @@ function addBit(y){
     }
 }
 
-function updateBit(input, y){
+function updateBit(input, y, l){
 
-    for(let i = 0; i < COLS; i++){
+    for(let i = 0; i < COLS + -l; i++){
 
         if (isMouseOverRect(i * rectWidth, y, rectWidth, rectWidth) && mouseClick == true){
 
@@ -151,5 +174,15 @@ function bitAddition(){
                     outputA[i] = true
                 }
         }
+    }    
+}
+
+function keyPressed(){
+
+    for(let i = 0; i < COLS; i++){
+
+    inputA[i] = false
+    inputB[i] = false
+    setting[i] = false
     }    
 }
